7b0479b23d15761aa845d4568a499976121e829b
dungry853 <129316318+dungry853@users.noreply.github.com>
2024-02-08 02:07:41 +0700

    Prettier Code

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,5 +1,5 @@
 const Course = require('../models/Course');
-const { multipleMongooseToObject} = require('../../util/mongoose');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
   search(req, res) {
     res.render('news');
@@ -9,9 +9,9 @@ class SiteController {
   }
   home(req, res, next) {
     Course.find({})
-      .then(courses => {
-        res.render('home',{
-          courses: multipleMongooseToObject(courses)
+      .then((courses) => {
+        res.render('home', {
+          courses: multipleMongooseToObject(courses),
         });
       })
       .catch(next);
